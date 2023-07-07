952fe944c54552ae0992135488c019f6b9df8504
Byangacademic <byang68@myseneca.ca>
2023-07-07 00:27:26 -0400

    part 4

--- server.js
+++ server.js
@@ -1,55 +1,286 @@
 /*********************************************************************************
- *  WEB322 – Assignment 2
+ *  WEB322 – Assignment 4
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
- *  Name: Benny Yang Student ID: 112654223 Date: June 1 2023
+ *  Name: Benny Yang Student ID: 112654223 Date: July 5 2023
  *
  * ONLINE (CYCLIC) LINK: https://lazy-tan-fox-belt.cyclic.app
  *
  ********************************************************************************/
 
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require("multer");
+const upload = multer();
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
 var storeService = require("./store-service");
+
+const exphbs = require("express-handlebars");
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    helpers: {
+      navLink: function (url, options) {
+        return (
+          '<li class="nav-item"><a ' +
+          (url == app.locals.activeRoute
+            ? ' class = "nav-item active" '
+            : ' class = "nav-link" ') +
+          ' href =" ' +
+          url +
+          '"> ' +
+          options.fn(this) +
+          "</a></li>"
+        );
+      },
+      equal: function (lvalue, rvalue, options) {
+        if (arguments.length < 3)
+          throw new Error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
+app.set("view engine", ".hbs");
+cloudinary.config({
+  cloud_name: "daf6jhhxk",
+  api_key: "588681381715112",
+  api_secret: "e3ulwVGfU2LOcMk657YybzpxVsY",
+  secure: true,
+});
+// Code given HERE
+app.use(function (req, res, next) {
+  let route = req.path.substring(1);
+  app.locals.activeRoute =
+    "/" +
+    (isNaN(route.split("/")[1])
+      ? route.replace(/\/(?!.*)/, "")
+      : route.replace(/\/(.*)/, ""));
+  app.locals.viewingCategory = req.query.category;
+  next();
+});
+// TO HERE
 
 app.use(express.static("public"));
 
 // Redirect to /about
 app.get("/", (req, res) => {
-  res.redirect("/about");
+  res.redirect("/shop");
 });
 
 // The about page
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+  res.render("about");
 });
 
-// shop route
-app.get("/shop", (req, res) => {
-  storeService
-    .getPublishedItems()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((error) => {
-      console.error("Failed to get categories:", error);
-      res.status(500).sendFile(__dirname + "/views/OOF.html");
-    });
+// shop route CODE GIVEN
+app.get("/shop", async (req, res) => {
+  // Declare an object to store properties for the view
+  let viewData = {};
+  try {
+    // declare empty array to hold objects
+    let items = [];
+
+    // if there's a "category" query, filter the returned posts by category
+    if (req.query.category) {
+      // Obtain the published "posts" by category
+      items = await storeService.getPublishedItemsByCategory(
+        req.query.category
+      );
+    } else {
+      // Obtain the published "items"
+      items = await storeService.getPublishedItems();
+    }
+
+    // sort the published items by postDate
+    items.sort((a, b) => new Date(b.postDate) - new Date(a.postDate));
+
+    // get the latest item from the front of the list (element 0)
+    let item = items[0];
+
+    // store the "items" and "item" data in the viewData object (to be passed to the view)
+    viewData.items = items;
+    viewData.item = item;
+  } catch (err) {
+    viewData.message = "no results";
+  }
+
+  try {
+    // Obtain the full list of "categories"
+    let categories = await storeService.getCategories();
+
+    // store the "categories" data in the viewData object (to be passed to the view)
+    viewData.categories = categories;
+  } catch (err) {
+    viewData.categoriesMessage = "no results";
+  }
+
+  // render the "shop" view with all of the data (viewData)
+  res.render("shop", { data: viewData });
 });
+
+app.get("/shop/:id", async (req, res) => {
+  // Declare an object to store properties for the view
+  let viewData = {};
+
+  try {
+    // declare empty array to hold "item" objects
+    let items = [];
+
+    // if there's a "category" query, filter the returned posts by category
+    if (req.query.category) {
+      // Obtain the published "posts" by category
+      items = await storeService.getPublishedItemsByCategory(
+        req.query.category
+      );
+    } else {
+      // Obtain the published "posts"
+      items = await storeService.getPublishedItems();
+    }
+
+    // sort the published items by postDate
+    items.sort((a, b) => new Date(b.postDate) - new Date(a.postDate));
+
+    // store the "items" and "item" data in the viewData object (to be passed to the view)
+    viewData.items = items;
+  } catch (err) {
+    viewData.message = "no results";
+  }
+
+  try {
+    // Obtain the item by "id"
+    viewData.item = await storeService.getItemById(req.params.id);
+  } catch (err) {
+    viewData.message = "no results";
+  }
+
+  try {
+    // Obtain the full list of "categories"
+    let categories = await storeService.getCategories();
+
+    // store the "categories" data in the viewData object (to be passed to the view)
+    viewData.categories = categories;
+  } catch (err) {
+    viewData.categoriesMessage = "no results";
+  }
+
+  // render the "shop" view with all of the data (viewData)
+  res.render("shop", { data: viewData });
+});
+// CODE GIVEN ENDS
 
 // items route
 app.get("/items", (req, res) => {
+  const category = req.query.category;
+  const minDate = req.query.minDate;
+
+  if (category) {
+    storeService
+      .getItemsByCategory(category)
+      .then((data) => {
+        res.render("items", { items: data });
+      })
+      .catch((error) => {
+        console.error("Failed to get items of selected category:", error);
+        res.render("items", { message: "no results" });
+      });
+  } else if (minDate) {
+    storeService
+      .getItemsByMinDate(minDate)
+      .then((data) => {
+        res.render("items", { items: data });
+      })
+      .catch((error) => {
+        console.error("Failed to get items of selected minDate:", error);
+        res.render("items", { message: "no results" });
+      });
+  } else {
+    storeService
+      .getAllItems()
+      .then((data) => {
+        res.render("items", { items: data });
+      })
+      .catch((error) => {
+        console.error("Failed to get categories:", error);
+        res.render("items", { message: "no results" });
+      });
+  }
+});
+
+// items add route
+app.get("/items/add", (req, res) => {
+  res.render("addItem");
+});
+
+//items add post
+app.post("/items/add", upload.single("featureImage"), (req, res) => {
+  // GIVEN CODE HERE
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+    upload(req).then((uploaded) => {
+      processItem(uploaded.url);
+    });
+  } else {
+    processItem("");
+  }
+
+  function processItem(imageUrl) {
+    req.body.featureImage = imageUrl;
+    // TODO: Process the req.body and add it as a new Item before redirecting to /items
+    // SELF ADDED CODE HERE
+    storeService
+      .addItem(req.body)
+      .then(() => {
+        res.redirect("/items");
+      })
+      .catch((err) => {
+        console.error("Failed to add item:", err);
+        res.render("items", { message: "no results" });
+      });
+  }
+});
+
+app.get("/item/:id", (req, res) => {
+  const itemId = req.params.id;
   storeService
-    .getAllItems()
+    .getItemById(itemId)
     .then((data) => {
-      res.send(data);
+      if (data) {
+        res.render("items", { items: data });
+      } else {
+        res.render("items", { message: "Item not found" });
+      }
     })
     .catch((error) => {
-      console.error("Failed to get categories:", error);
-      res.status(500).sendFile(__dirname + "/views/OOF.html");
+      console.error("Failed to get item of this id:", error);
+      res.render("items", { message: "no results" });
     });
 });
 
@@ -58,16 +289,17 @@ app.get("/categories", (req, res) => {
   storeService
     .getCategories()
     .then((data) => {
-      res.send(data);
+      res.render("categories", { categories: data });
     })
     .catch((error) => {
       console.error("Failed to get categories:", error);
-      res.status(500).sendFile(__dirname + "/views/OOF.html");
+      res.render("categories", { message: "no results" });
     });
 });
 
+// CATCH ALL
 app.get("*", (req, res) => {
-  res.status(404).sendFile(__dirname + "/views/404.html");
+  res.status(404).render("404");
 });
 
 // Setup http server to listen to HTTP_PORT
--- store-service.js
+++ store-service.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
- *  WEB322 – Assignment 2
+ *  WEB322 – Assignment 4
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
- *  Name: Benny Yang Student ID: 112654223 Date: June 1 2023
+ *  Name: Benny Yang Student ID: 112654223 Date: July 5 2023
  *
  * ONLINE (CYCLIC) LINK: https://lazy-tan-fox-belt.cyclic.app
  *
@@ -65,10 +65,82 @@ function getPublishedItems() {
 function getCategories() {
   return new Promise((resolve, reject) => {
     if (categories.length === 0) {
-        reject("No category results returned");
-        return;
+      reject("No category results returned");
+      return;
     }
     resolve(categories);
+  });
+}
+
+function getItemsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    if (items.length === 0) {
+      reject("No items results returned");
+      return;
+    }
+    let results = items.filter((item) => item.category == category);
+    if (results.length === 0) {
+      reject("Nothing Found here babe");
+    }
+    resolve(results);
+  });
+}
+
+function getItemsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    if (items.length === 0) {
+      reject("No items results returned");
+      return;
+    }
+
+    let results = items.filter(
+      (item) => new Date(item.postDate) >= new Date(minDateStr)
+    );
+    if (results.length === 0) {
+      reject("Nothing Found here babe");
+    }
+    resolve(results);
+  });
+}
+
+function getItemById(id) {
+  return new Promise((resolve, reject) => {
+    let result = items.find((item) => item.id == id);
+    if (result) {
+      resolve(result);
+    } else {
+      reject("Item not found");
+    }
+  });
+}
+
+function addItem(itemData) {
+  return new Promise((resolve, reject) => {
+    itemData.published = itemData.published == undefined ? false : true;
+    itemData.id = items.length + 1;
+    let date = new Date();
+    // Get year, month, and day part from the date
+    var year = date.toLocaleString("default", { year: "numeric" });
+    var month = date.toLocaleString("default", { month: "2-digit" });
+    var day = date.toLocaleString("default", { day: "2-digit" });
+
+    // Generate yyyy-mm-dd date string
+    itemData.postDate = year + "-" + month + "-" + day;
+    items.push(itemData);
+    resolve(items);
+  });
+}
+
+function getPublishedItemsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    let publishedCatItems = items.filter(
+      (item) => item.published == true && item.category == category
+    );
+    if (publishedCatItems.length === 0) {
+      reject("No published item results returned");
+      return;
+    }
+    resolve(publishedCatItems);
   });
 }
 
@@ -77,4 +149,9 @@ module.exports = {
   getAllItems,
   getPublishedItems,
   getCategories,
+  getItemsByCategory,
+  getItemsByMinDate,
+  getItemById,
+  addItem,
+  getPublishedItemsByCategory,
 };
